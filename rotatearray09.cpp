f1dc5b62d71f0005df7d8642bdbc4036c2184dc3
Md Imran Shakeel <imshkl2005@gmail.com>
2026-01-26 15:59:44 +0530

    Enhance rotatearray function with comments
    Added comments to clarify the rotation logic and output.

--- rotatearray09.cpp
+++ rotatearray09.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 using namespace std;
-void rotatearray(int arr[],int n,int k){
-    int temp[n];
+void rotatearray(int arr[], int n, int k){
+    int temp[n];                  // temporary array of same size
+
     for(int i=0;i<n;i++){
-    temp[(i+k)%n]=arr[i];
+        temp[(i+k)%n] = arr[i];   // place each element at its new rotated index
     }
+
     for(int i=0;i<n;i++){
-        arr[i]=temp[i];
+        arr[i] = temp[i];        // copy rotated elements back to original array
     }
 }
+
 void printarray(int arr[],int n){
     for(int i=0; i<n;i++){
         cout<<arr[i]<<" ";
@@ -23,4 +26,5 @@ int main(){
     return 0;
 }
 // OUTPUT-
+
 // 4 5 1 2 3 
