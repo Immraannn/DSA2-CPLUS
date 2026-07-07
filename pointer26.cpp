5432f9dbe4c3e331135a5857d756cb4013107187
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-07 20:58:14 +0530

    Refactor pointer26.cpp with detailed comments
    Updated pointer operations and added comments for clarity.

--- pointer26.cpp
+++ pointer26.cpp
@@ -1,27 +1,68 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int num=5;
-    int a=num;
-    cout<<"a before "<<a<<endl;
+
+    // Integer variable
+    int num = 5;
+
+    // Copy the value of num into a
+    int a = num;
+
+    cout << "a before " << a << endl;
+
+    // Increment a
+    // Only 'a' changes, num remains unchanged
     a++;
-    cout<<"a after "<<a<<endl;
-    int*p=&num;
-    cout<<"before "<<num<<endl;
+
+    cout << "a after " << a << endl;
+
+    // Pointer storing the address of num
+    int *p = &num;
+
+    cout << "before " << num << endl;
+
+    // Dereference the pointer and increment the value
+    // This changes the actual value of num
     (*p)++;
-    cout<<"after "<<num<<endl;
-    //COPY a POINTER
-    int*q=p;
-    cout<<p<<endl;
-    cout<<q<<endl;
-    //POINTER ARITHEMETIC
-    int  i=3;
-    int*t=&i;
-    *t=*t+1;
-    cout<<*t<<endl;
-    cout<<"before t "<<t<<endl;
-    t=t+1;
-    cout<<"after t:"<<t<<endl;
+
+    cout << "after " << num << endl;
+
+    // ------------------------------
+    // Copying a Pointer
+    // ------------------------------
+
+    // q stores the same address as p
+    int *q = p;
+
+    // Both pointers point to the same variable (num)
+    cout << p << endl;
+    cout << q << endl;
+
+    // ------------------------------
+    // Pointer Arithmetic
+    // ------------------------------
+
+    int i = 3;
+
+    // Pointer pointing to i
+    int *t = &i;
+
+    // Increment the value stored at address t
+    *t = *t + 1;
+
+    cout << *t << endl;
+
+    // Print address stored in pointer t
+    cout << "before t " << t << endl;
+
+    // Move pointer to the next integer location
+    // Since int occupies 4 bytes,
+    // the address increases by 4 bytes
+    t = t + 1;
+
+    cout << "after t: " << t << endl;
+
     return 0;
 }
 // OUTPUT-
