308442cbb1fc0380592ca38c09734f852b3191d8
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-07 20:30:45 +0530

    Refactor GCD calculation and enhance user prompts
    Refactor GCD function and improve input/output prompts.

--- GCD24.cpp
+++ GCD24.cpp
@@ -1,24 +1,50 @@
 #include<iostream>
 using namespace std;
-int gcd(int a,int b){
-    if(a==0) return b;
-    if (b==0) return a;
-    while(a!=b){
-        if(a>b){
-            a=a-b;
+
+// Function to find GCD of two numbers
+int gcd(int a, int b){
+
+    // If first number is 0, GCD is the second number
+    if(a == 0)
+        return b;
+
+    // If second number is 0, GCD is the first number
+    if(b == 0)
+        return a;
+
+    // Continue until both numbers become equal
+    while(a != b){
+
+        // If a is greater, subtract b from a
+        if(a > b){
+            a = a - b;
         }
+
+        // Otherwise subtract a from b
         else{
-            b=b-a;
+            b = b - a;
         }
     }
+
+    // When both numbers become equal,
+    // that value is the GCD
     return a;
 }
+
 int main(){
-    int a,b;
-    cout<<"Enter the vslkues of a and b "<<endl;
-    cin>>a>>b;
-    int ans=gcd(a,b);
-    cout<<"The gcd of "<<a<<" and "<<b<<" is "<<ans<<endl;
+
+    int a, b;
+
+    // Input two numbers
+    cout << "Enter the values of a and b: " << endl;
+    cin >> a >> b;
+
+    // Call gcd function
+    int ans = gcd(a, b);
+
+    // Print the result
+    cout << "The GCD of " << a << " and " << b << " is " << ans << endl;
+
     return 0;
 }
 // OUTPUT-
