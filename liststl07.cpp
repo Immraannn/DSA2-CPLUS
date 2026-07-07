5053ced695cd9074dc0b6c71f95a1fcf40185482
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-07 18:01:00 +0530

    Refactor liststl07.cpp for improved readability
    Updated comments and output messages for clarity.

--- liststl07.cpp
+++ liststl07.cpp
@@ -1,28 +1,52 @@
 #include<iostream>
-#include<list>
+#include<list>        // Header file for STL list
 using namespace std;
+
 int main(){
-    list<int>l;
-    list<int>n(5,100);
-    cout<<"Printing n "<<endl;
-    for(int i:n){
-        cout<<i<<" ";
+
+    // Create an empty doubly linked list
+    list<int> l;
+
+    // Create a list of size 5 with all elements initialized to 100
+    list<int> n(5,100);
+
+    // Print the elements of list 'n'
+    cout << "Printing n" << endl;
+    for(int i : n){
+        cout << i << " ";
     }
-    cout<<endl;
+    cout << endl;
+
+    // Insert 1 at the end of the list
     l.push_back(1);
+
+    // Insert 2 at the beginning of the list
     l.push_front(2);
-    for(int i:l){
-        cout<<i<<" ";
+
+    // Print the list
+    cout << "Elements of list l:" << endl;
+    for(int i : l){
+        cout << i << " ";
     }
-    cout<<endl;
-    cout<<"size of list before erase "<<l.size()<<endl;
+    cout << endl;
+
+    // Print current size of the list
+    cout << "Size of list before erase: " << l.size() << endl;
+
+    // Remove the first element
+    // begin() returns an iterator pointing to the first element
     l.erase(l.begin());
-    cout<<"after erase erase "<<endl;
-    for(int i:l){
-        cout<<i<<" ";
+
+    // Print the list after erasing the first element
+    cout << "After erase:" << endl;
+    for(int i : l){
+        cout << i << " ";
     }
-    cout<<endl;
-    cout<<"size of list after erase "<<l.size()<<endl;
+    cout << endl;
+
+    // Print size after erase
+    cout << "Size of list after erase: " << l.size() << endl;
+
     return 0;
 }
 // OUTPUT-
