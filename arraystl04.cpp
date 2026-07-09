6fedb0b766059485c71895382c42d6c8d5931597
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-09 11:40:38 +0530

    Refactor array example and add swap and iterator usage

--- arraystl04.cpp
+++ arraystl04.cpp
@@ -21,3 +21,51 @@ int main(){
 // Empty or not 0
 // First element 1
 // last element 4
+//SWAP
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    array<int,3> a = {1,2,3};
+    array<int,3> b = {10,20,30};
+
+    a.swap(b);
+
+    cout << "Array A : ";
+
+    for(int x : a)
+        cout << x << " ";
+
+    cout << endl;
+
+    cout << "Array B : ";
+
+    for(int x : b)
+        cout << x << " ";
+
+    return 0;
+}
+//output
+// Array A : 10 20 30
+// Array B : 1 2 3
+
+//ITERATOR
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main()
+{
+    array<int,5> arr = {10,20,30,40,50};
+
+    for(auto it = arr.begin(); it != arr.end(); it++)
+    {
+        cout << *it << " ";
+    }
+
+    return 0;
+}
+//output
+10 20 30 40 50
