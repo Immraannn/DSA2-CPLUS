8dac875f2e923dbb7c1b4f336aad8b0a2686bbde
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 16:53:34 +0530

    Refactor largestRow function and add traverse method

--- largestrow19.cpp
+++ largestrow19.cpp
@@ -1,3 +1,23 @@
+
+
+class Solution {
+public:
+    void traverse(vector<vector<int>>& matrix) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+
 class Solution {
 public:
     int largestRow(vector<vector<int>>& matrix) {
