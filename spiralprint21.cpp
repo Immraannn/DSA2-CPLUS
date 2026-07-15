7af3ce9564021d0881e107a713757d211ed24e50
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 17:00:54 +0530

    Update spiralprint21.cpp

--- spiralprint21.cpp
+++ spiralprint21.cpp
@@ -1,52 +1,42 @@
-#include<iostream>
-using namespace std;
-void spiralprint(int arr[][3],int r,int c){
-    int count=0;
-    int total=r*c;
-    int startingrow=0;
-    int startingcol=0;
-    int endingrow=r-1;
-    int endingcol=c-1;
-    while(count<total){
-        //printing starting row
-        for(int index=startingcol;count<total &&index<=endingcol;index++){
-            cout<<arr[startingrow][index]<<" ";
-            count++;
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int top = 0;
+        int bottom = rows - 1;
+        int left = 0;
+        int right = cols - 1;
+        while (top <= bottom && left <= right) {
+            // Top Row (Left -> Right)
+            for (int j = left; j <= right; j++) {
+                ans.push_back(matrix[top][j]);
+            }
+            top++;
+            // Right Column (Top -> Bottom)
+            for (int i = top; i <= bottom; i++) {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            // Bottom Row (Right -> Left)
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--) {
+                    ans.push_back(matrix[bottom][j]);
+                }
+                bottom--;
+            }
+            // Left Column (Bottom -> Top)
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
         }
-        startingrow++;
-        //printing ending column
-        for(int index=startingrow; count<total &&index<=endingrow;index++){
-            cout<<arr[index][endingcol]<<" ";
-            count++;
-        }
-        endingcol--;
-        //printing ending row
-        for(int index=endingcol; count<total &&index>=startingcol;index--){
-            cout<<arr[endingrow][index]<<" ";
-            count++;
-        }
-        endingrow--;
-        //printing starting column
-        for(int index=endingrow; count<total &&index>=startingrow;index--){
-            cout<<arr[index][startingcol]<<" ";
-            count++;
-        }
-        startingcol++;
+        return ans;
     }
-}
-
-int main(){
-    int arr[3][3];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cin>>arr[i][j];
-        }
-    }
-cout<<"The wave print is :"<<endl;
-    spiralprint(arr,3,3);
-
-return 0;
-}
+};
 // OUTPUT-
 // 1 2 3
 // 4 5 6
