3d96f85d0ac5ee8a6328a2f33df5f0d5b186a12e
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 17:05:43 +0530

    Update rotatematrix90_22.cpp

--- rotatematrix90_22.cpp
+++ rotatematrix90_22.cpp
@@ -1,84 +1,43 @@
-#include<iostream>
-using namespace std;
-
-// Transpose the matrix
-void transpose(int arr[][3], int n){
-
-    for(int i = 0; i < n; i++){
-
-        for(int j = i + 1; j < n; j++){
-
-            swap(arr[i][j], arr[j][i]);
+class Solution {
+public:
+//90 clock wise reverse row
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // Step 1: Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
-}
+};
 
-// Reverse every row
-void reverseRows(int arr[][3], int n){
 
-    for(int i = 0; i < n; i++){
+//90 anti clock wise
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        // Reverse each column
+        for (int j = 0; j < n; j++) {
+            int top = 0;
+            int bottom = n - 1;
 
-        int s = 0;
-        int e = n - 1;
-
-        while(s < e){
-
-            swap(arr[i][s], arr[i][e]);
-
-            s++;
-            e--;
+            while (top < bottom) {
+                swap(matrix[top][j], matrix[bottom][j]);
+                top++;
+                bottom--;
+            }
         }
     }
-}
-
-// Print matrix
-void printMatrix(int arr[][3], int n){
-
-    for(int i = 0; i < n; i++){
-
-        for(int j = 0; j < n; j++){
-
-            cout << arr[i][j] << " ";
-        }
-
-        cout << endl;
-    }
-}
-
-int main(){
-
-    int arr[3][3];
-
-    cout << "Enter matrix:" << endl;
-
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            cin >> arr[i][j];
-        }
-    }
-
-    // Step 1
-    transpose(arr, 3);
-
-    // Step 2
-    reverseRows(arr, 3);
-
-    cout << "\nMatrix after 90 degree rotation:\n";
-
-    printMatrix(arr, 3);
-
-    return 0;
-}
-
-// original matrix
-// 1 2 3
-// 4 5 6
-// 7 8 9
-// after transpose
-// 1 4 7
-// 2 5 8
-// 3 6 9
-//  after reversing each row
-// 7 4 1
-// 8 5 2
-// 9 6 3
+};
