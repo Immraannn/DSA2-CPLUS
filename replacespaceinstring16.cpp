c0342d5533c0ebff72d47a58cb253d56926650aa
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 16:42:02 +0530

    Update replacespaceinstring16.cpp

--- replacespaceinstring16.cpp
+++ replacespaceinstring16.cpp
@@ -1,28 +1,24 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main() {
-    string str, result = "";
-    cout << "Enter a string: ";
-    getline(cin, str);
-
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] == ' ') {
-            result += "@40";
-        } else {
-            result += str[i];
+class Solution {
+public:
+    string replaceSpaces(string s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ' ') {
+                s[i] = '#';
+            }
         }
+        return s;
     }
+};
 
-    cout << "Modified string: " << result << endl;
-    return 0;
-}
-
-
-
-// OUTPUT-
-// Enter a string: my name is imran
-// Modified string: my@40name@40is@40imran
-
+class Solution {
+public:
+    string replaceSpaces(string s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ' ') {
+                s.replace(i, 1, "@#");  //s.replace(position, number_of_characters_to_remove, new_string);
+                i++;   // Skip the newly inserted '#'
+            }
+        }
+        return s;
+    }
+};
