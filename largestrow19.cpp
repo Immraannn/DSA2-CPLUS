7d0d4a5b677c155e91957ef16a99b20d3c4c0007
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 16:51:57 +0530

    Update largestrow19.cpp

--- largestrow19.cpp
+++ largestrow19.cpp
@@ -1,44 +1,51 @@
-#include<iostream>
-using namespace std;
-int  largestrow(int arr[][4],int r,int c){
-    int max=INT8_MIN;
-    int rowindex=-1;
-    for(int i=0;i<3;i++){
-        int sum=0;
-        for(int j=0;j<4;j++){
-            sum +=arr[i][j];
+class Solution {
+public:
+    int largestRow(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int maxSum = INT_MIN;
+        int rowIndex = -1;
+        for (int i = 0; i < rows; i++) {
+            int sum = 0;
+            for (int j = 0; j < cols; j++) {
+                sum += matrix[i][j];
             }
-            if(sum>max){
-                max=sum;
-                rowindex=i;
+            if (sum > maxSum) {
+                maxSum = sum;
+                rowIndex = i;
             }
         }
-    cout<<"The maximum sum is :"<<max<<endl;
-    cout<<"The row index is:";
-    return rowindex;
+        return rowIndex;   // Returns index of the row with the largest sum
     }
+};
 
-int main(){
-    int arr[3][4];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){
-            cin>>arr[i][j];
-        }
-    }
 
-    for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){
-            cout<<arr[i][j]<<" ";
+class Solution {
+public:
+    pair<int, int> largestRow(vector<vector<int>>& matrix) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        int maxSum = INT_MIN;
+        int rowIndex = -1;
+
+        for (int i = 0; i < rows; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < cols; j++) {
+                sum += matrix[i][j];
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                rowIndex = i;
+            }
         }
-        cout<<endl;
+
+        return {rowIndex, maxSum};
     }
-cout<<largestrow(arr,3,4);
-return 0;
-}
-// OUTPUT-
-// 11 12 13 14 10 20 30 40 11 23 34 22            
-// 11 12 13 14
-// 10 20 30 40
-// 11 23 34 22
-// The maximum sum is :100
-// The row index is:1
+};
+
+
+
