85aa3b7d0bf18072745ba99e7f06e36429b62872
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 15:28:54 +0530

    Update mergesortedarrar08.cpp

--- mergesortedarrar08.cpp
+++ mergesortedarrar08.cpp
@@ -1,70 +1,75 @@
-#include<iostream>
-using namespace std;
-void merge(int arr1[], int n, int arr2[], int m, int arr3[]) {
 
-    // i -> pointer for arr1
-    // j -> pointer for arr2
-    // k -> pointer for arr3 (merged array)
-    int i = 0, j = 0, k = 0;
+class Solution {
+public:
+    vector<int> mergeSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-    // Run until any one array becomes empty
-    while(i < n && j < m) {
+        vector<int> ans;
 
-        // Compare current elements of both arrays
-        if(arr1[i] < arr2[j]) {
+        int i = 0;
+        int j = 0;
 
-            // Put smaller element from arr1 into arr3
-            arr3[k] = arr1[i];
+        while (i < nums1.size() && j < nums2.size()) {
 
-            // Move arr1 pointer forward
+            if (nums1[i] <= nums2[j]) {
+                ans.push_back(nums1[i]);
+                i++;
+            } else {
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        while (i < nums1.size()) {
+            ans.push_back(nums1[i]);
             i++;
-
-            // Move merged array pointer forward
-            k++;
         }
-        else {
 
-            // Put smaller element from arr2 into arr3
-            arr3[k] = arr2[j];
-
-            // Move arr2 pointer forward
+        while (j < nums2.size()) {
+            ans.push_back(nums2[j]);
             j++;
-
-            // Move merged array pointer forward
-            k++;
         }
-    }
 
-    // If any elements are left in arr1, copy them
-    while(i < n) {                 // until arr1 finishes
-        arr3[k] = arr1[i];        // copy element
-        i++;                      // move arr1 pointer
-        k++;                      // move arr3 pointer
+        return ans;
     }
+};
 
-    // If any elements are left in arr2, copy them
-    while(j < m) {                 // until arr2 finishes
-        arr3[k] = arr2[j];        // copy element
-        j++;                      // move arr2 pointer
-        k++;                      // move arr3 pointer
+
+class Solution {
+public:
+    void mergeSortedArray(vector<int>& nums1, vector<int>& nums2) {
+
+        // Store the merged array
+        vector<int> ans;
+
+        int i = 0;
+        int j = 0;
+
+        // Merge both arrays
+        while (i < nums1.size() && j < nums2.size()) {
+
+            if (nums1[i] <= nums2[j]) {
+                ans.push_back(nums1[i]);
+                i++;
+            }
+            else {
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+
+        // Copy remaining elements of nums1
+        while (i < nums1.size()) {
+            ans.push_back(nums1[i]);
+            i++;
+        }
+
+        // Copy remaining elements of nums2
+        while (j < nums2.size()) {
+            ans.push_back(nums2[j]);
+            j++;
+        }
+
+        // Copy the merged array back into nums1
+        nums1 = ans;
     }
-}
-
-void print(int arr[],int n){
-    for(int i=0; i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-}
-int main(){
-    int arr1[5]={1,3,5,7,9};
-    int arr2[3]={2,4,6};
-    int arr3[8];
-    merge(arr1,5,arr2,3,arr3);
-    cout<<"The merge two sorted array is "<<endl;
-    print(arr3,8);
-    return 0;
-}
-// OUTPUT-
-// The merge two sorted array is 
-
-// 1 2 3 4 5 6 7 9
+};
