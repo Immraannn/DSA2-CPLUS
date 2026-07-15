86d52c4008ee3fad2003929c8f31cb5c6b4980ab
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 15:33:52 +0530

    Update rotatearray09.cpp

--- rotatearray09.cpp
+++ rotatearray09.cpp
@@ -16,34 +16,19 @@ public:
 };
 
 
+class Solution {
+public:
+    vector<int> rotate(vector<int>& nums, int k) {
 
-#include<iostream>
-using namespace std;
-void rotatearray(int arr[], int n, int k){
-    int temp[n];                  // temporary array of same size
+        int n = nums.size();
+        k = k % n;
 
-    for(int i=0;i<n;i++){
-        temp[(i+k)%n] = arr[i];   // place each element at its new rotated index
+        vector<int> ans(n);
+
+        for (int i = 0; i < n; i++) {
+            ans[(i + k) % n] = nums[i];
+        }
+
+        return ans;// for return type
     }
-
-    for(int i=0;i<n;i++){
-        arr[i] = temp[i];        // copy rotated elements back to original array
-    }
-}
-
-void printarray(int arr[],int n){
-    for(int i=0; i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-}
-int main(){
-    int n=5;
-    int arr[n]={1,2,3,4,5};
-    int temp[n];
-    rotatearray(arr,n,2);
-     printarray(arr,n);
-    return 0;
-}
-// OUTPUT-
-
-// 4 5 1 2 3 
+};
