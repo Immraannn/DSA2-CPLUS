4cb463d09c3665c516fad8902c0b1b210d594957
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 16:58:17 +0530

    Update waveprint20.cpp

--- waveprint20.cpp
+++ waveprint20.cpp
@@ -1,35 +1,74 @@
-#include<iostream>
-using namespace std;
-void waveprint(int arr[][4],int r,int c){
-    for(int j=0;j<c;j++){
-        if(j%2==1){//odd index->bottom to top
-for(int i=r-1;i>=0;i--){
-    cout<<arr[i][j]<<" ";
-}
-        }
-        else{// even index->top to bottom
-            for(int i=0;i<r;i++){
-    cout<<arr[i][j]<<" ";
-}
-        }
-    }
-}
+// In Wave Print, you print:
 
-int main(){
-    int arr[3][4];
-    for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){
-            cin>>arr[i][j];
-        }
-    }
-cout<<"The wave print is :"<<endl;
-    waveprint(arr,3,4);
+// 1st column: Top → Bottom
+// 2nd column: Bottom → Top
+// 3rd column: Top → Bottom
+// and so on.
 
-return 0;
-}
-// OUTPUT-
-// 1 2 3 4
-// 5 6 7 8
-// 9 8 7 6
-// The wave print is :
-// 1 5 9 8 6 2 3 7 7 6 8 4
+class Solution {
+public:
+    vector<int> wavePrint(vector<vector<int>>& matrix) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        vector<int> ans;
+
+        for (int j = 0; j < cols; j++) {
+
+            // Even column: Top to Bottom
+            if (j % 2 == 0) {
+                for (int i = 0; i < rows; i++) {
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+            // Odd column: Bottom to Top
+            else {
+                for (int i = rows - 1; i >= 0; i--) {
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+// Row-wise Wave Print
+
+// Print:
+
+// 1st row: Left → Right
+// 2nd row: Right → Left
+// 3rd row: Left → Right
+// and so on.
+
+class Solution {
+public:
+    vector<int> wavePrint(vector<vector<int>>& matrix) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        vector<int> ans;
+
+        for (int i = 0; i < rows; i++) {
+
+            // Even row: Left to Right
+            if (i % 2 == 0) {
+                for (int j = 0; j < cols; j++) {
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+            // Odd row: Right to Left
+            else {
+                for (int j = cols - 1; j >= 0; j--) {
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
