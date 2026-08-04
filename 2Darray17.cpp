31e978e129cf76b5b7bdc9cd4b336609022c3bb3
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-04 22:11:17 +0530

    Update 2Darray17.cpp

--- 2Darray17.cpp
+++ 2Darray17.cpp
@@ -1,3 +1,23 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+
+
+
 #include<iostream>
 using namespace std;
 bool ispresent(int arr[][4],int target ,int r,int c ){
