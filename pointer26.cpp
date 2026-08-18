6b85f0ad992a8bbd9facec0594e2da53b6dd0c4b
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-18 18:12:37 +0530

    Update pointer26.cpp

--- pointer26.cpp
+++ pointer26.cpp
@@ -1,61 +1,41 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-
     // Integer variable
     int num = 5;
-
     // Copy the value of num into a
     int a = num;
-
     cout << "a before " << a << endl;
-
     // Increment a
     // Only 'a' changes, num remains unchanged
     a++;
-
     cout << "a after " << a << endl;
-
     // Pointer storing the address of num
     int *p = &num;
-
     cout << "before " << num << endl;
-
     // Dereference the pointer and increment the value
     // This changes the actual value of num
     (*p)++;
-
     cout << "after " << num << endl;
-
     // ------------------------------
     // Copying a Pointer
     // ------------------------------
-
     // q stores the same address as p
     int *q = p;
-
     // Both pointers point to the same variable (num)
     cout << p << endl;
     cout << q << endl;
-
     // ------------------------------
     // Pointer Arithmetic
     // ------------------------------
-
     int i = 3;
-
     // Pointer pointing to i
     int *t = &i;
-
     // Increment the value stored at address t
     *t = *t + 1;
-
     cout << *t << endl;
-
     // Print address stored in pointer t
     cout << "before t " << t << endl;
-
     // Move pointer to the next integer location
     // Since int occupies 4 bytes,
     // the address increases by 4 bytes
