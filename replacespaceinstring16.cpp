f913b4b42b645ea60036ddc3b437b1e004198c6c
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-18 10:29:57 +0530

    Refactor replaceSpaces to use string.replace method
    Refactored the replaceSpaces function to use string.replace for better handling of spaces.

--- replacespaceinstring16.cpp
+++ replacespaceinstring16.cpp
@@ -16,7 +16,7 @@ public:
         for (int i = 0; i < s.length(); i++) {
             if (s[i] == ' ') {
                 s.replace(i, 1, "@#");  //s.replace(position, number_of_characters_to_remove, new_string);
-                i++;   // Skip the newly inserted '#'
+               // i++;   // Skip the newly inserted '#'
             }
         }
         return s;
