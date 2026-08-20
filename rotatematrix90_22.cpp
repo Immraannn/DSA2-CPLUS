3e6c19f0890ee28b297cce8972680a655d663549
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-20 23:23:19 +0530

    Update rotatematrix90_22.cpp

--- rotatematrix90_22.cpp
+++ rotatematrix90_22.cpp
@@ -10,8 +10,14 @@ public:
             }
         }
         // Step 2: Reverse each row
-        for (int i = 0; i < n; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
+     for(int i=0;i<n;i++){
+           int s=0;
+           int e=n-1;
+           while(s<e){
+            swap(matrix[i][s],matrix[i][e]);
+            s++;
+            e--;
+           }
         }
     }
 };
