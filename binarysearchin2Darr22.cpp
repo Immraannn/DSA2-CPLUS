efefbdc971f481762c1699596c590782e4a20d1f
Md Imran Shakeel <imshkl2005@gmail.com>
2026-09-09 21:29:07 +0530

    Refactor searchMatrix function in Solution class

--- binarysearchin2Darr22.cpp
+++ binarysearchin2Darr22.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-
         if (matrix.empty() || matrix[0].empty())
             return false;
-
         int rows = matrix.size();
         int cols = matrix[0].size();
-
         int start = 0;
         int end = rows * cols - 1;
-
         while (start <= end) {
-
             int mid = start + (end - start) / 2;
-
             int row = mid / cols;
             int col = mid % cols;
-
             if (matrix[row][col] == target)
                 return true;
-
             if (matrix[row][col] < target)
                 start = mid + 1;
             else
                 end = mid - 1;
         }
-
         return false;
     }
 };
