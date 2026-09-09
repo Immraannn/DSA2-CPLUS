4a3300338e7980354854ced38d4c0ade72354b60
Md Imran Shakeel <imshkl2005@gmail.com>
2026-09-09 19:37:27 +0530

    Update sumof2arrays10.cpp

--- sumof2arrays10.cpp
+++ sumof2arrays10.cpp
@@ -5,25 +5,19 @@ public:
         int i = a.size() - 1;
         int j = b.size() - 1;
         int carry = 0;
-        // Continue until both arrays and carry are exhausted
         while (i >= 0 || j >= 0 || carry) {
             int sum = carry;
-            // Add digit from first array
             if (i >= 0) {
                 sum += a[i];
                 i--;
             }
-            // Add digit from second array
             if (j >= 0) {
                 sum += b[j];
                 j--;
             }
-            // Store the last digit
             ans.push_back(sum % 10);
-       // Update carry
             carry = sum / 10;
         }
-        // Digits are stored in reverse order
         reverse(ans.begin(), ans.end());
         return ans;
     }
