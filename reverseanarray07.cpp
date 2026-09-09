bca6b0ab89df2ffec9e716a51330f1c525fd32a1
Md Imran Shakeel <imshkl2005@gmail.com>
2026-09-09 19:30:31 +0530

    Update reverseanarray07.cpp

--- reverseanarray07.cpp
+++ reverseanarray07.cpp
@@ -3,36 +3,15 @@ public:
     void reverseArray(vector<int>& nums) {
         int left = 0;
         int right = nums.size() - 1;
-        // Continue until both pointers meet
-        while (left < right) {
-            // Swap the left and right elements
-            swap(nums[left], nums[right]);
-            // Move left pointer forward
-            left++;
-            // Move right pointer backward
-            right--;
-        }
-    }
-};
-
-
-
-
-class Solution {
-public:
-    vector<int> reverseArray(vector<int>& nums) {
-        int left = 0;
-        int right = nums.size() - 1;
-        // Swap elements from both ends
         while (left < right) {
             swap(nums[left], nums[right]);
             left++;
             right--;
         }
-        // Return the reversed vector
-        return nums;
+        // return nums;for return type
     }
 };
+
 
 | Return Type           | Returns               | Example                        |
 | --------------------- | --------------------- | ------------------------------ |
